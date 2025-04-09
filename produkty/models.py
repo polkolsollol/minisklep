e4d5f690ae641a038241a864db09a3cad6676b33
polkolsollol <kuba.bochenski@vp.pl>
2025-04-09 11:12:54 +0200

    Dodanie modelu Produkt oraz wykonanie migracji

--- produkty/models.py
+++ produkty/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Produkt(models.Model):
+    nazwa = models.CharField(max_length=100)
+    cena = models.DecimalField(max_digits=8, decimal_places=2)
+    data_dodania = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nazwa
