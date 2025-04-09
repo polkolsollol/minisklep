1fa14ff8feb982f21b3f16e9f93099968c6d149e
polkolsollol <kuba.bochenski@vp.pl>
2025-04-09 11:25:58 +0200

    Stworzenie podstawowego panelu administracyjnego

--- mini_sklep/urls.py
+++ mini_sklep/urls.py
@@ -21,6 +21,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('produkty.urls')),
+    path('panel/', include('admin_panel.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
